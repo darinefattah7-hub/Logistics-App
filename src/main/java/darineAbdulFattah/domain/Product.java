4e04ce0a3f6aa7e13b1301076d1606a70d770c52
Darine Abdul Fattah <darinefattah7@gmail.com>
2026-05-15 00:35:26 +0300

    Update Product.java

--- src/main/java/darineAbdulFattah/domain/Product.java
+++ src/main/java/darineAbdulFattah/domain/Product.java
@@ -1,25 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package darineAbdulFattah.domain;
 
-/**
- *
- * @author USER
- */
 public class Product implements Comparable<Product> {
     private static int idCounter = 1;
     private final int id; 
     private String name;
     private Measurement weight;
     
-    
-     public Product(String name, Measurement weight) {
+    public Product(String name, Measurement weight) {
         this.id = idCounter++;
         this.name = name;
-        this.weight = new Measurement(weight.getValue(), weight.getUnit()); 
+        this.weight = weight.copy(); 
     }
+
     @Override
     public String toString() {
         return name + "," + weight.toString();
@@ -27,25 +19,36 @@ public class Product implements Comparable<Product> {
     
     @Override
     public int compareTo(Product other) {
-        // First compare by weight value
-        int weightComparison = Double.compare(this.weight.getValue(), 
-                                             other.weight.getValue());
+        int weightComparison = Double.compare(this.weight.value(), other.weight.value());
         
         if (weightComparison != 0) {
             return weightComparison;
         }
         
-        // If weights equal, compare by name (case-insensitive)
         return this.name.compareToIgnoreCase(other.name);
     }
     
-    // Package-private accessors for ProductList
-    public String getName() {
-        return name;
+    boolean hasName(String productName) {
+        return name.equalsIgnoreCase(productName);
     }
-    
-    public Measurement getWeight() {
-        return weight;
+
+    boolean hasSameName(Product other) {
+        return hasName(other.name);
     }
-    
+
+    void addWeightFrom(Product other) {
+        weight.addMeasurement(other.weight);
+    }
+
+    public boolean hasAtLeast(Measurement requested) {
+        return weight.hasSameUnit(requested) && weight.value() >= requested.value();
+    }
+
+    public boolean hasLessThan(Measurement requested) {
+        return weight.hasSameUnit(requested) && weight.value() < requested.value();
+    }
+
+    public String quantityString() {
+        return weight.toString();
+    }
 }
