4a0a3290fa348cb81c4cdac65b74e5f38fad1c59
Darine Abdul Fattah <darinefattah7@gmail.com>
2026-05-15 00:36:06 +0300

    Update ProductList.java

--- src/main/java/darineAbdulFattah/domain/ProductList.java
+++ src/main/java/darineAbdulFattah/domain/ProductList.java
@@ -1,6 +1,7 @@
 package darineAbdulFattah.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 public class ProductList extends ArrayList<Product> {
@@ -12,17 +13,26 @@ public class ProductList extends ArrayList<Product> {
     @Override
     public boolean add(Product newProduct) {
         for (Product existingProduct : this) {
-            if (existingProduct.getName().equalsIgnoreCase(newProduct.getName())) {
-                existingProduct.getWeight().addMeasurement(newProduct.getWeight());
+            if (existingProduct.hasSameName(newProduct)) {
+                existingProduct.addWeightFrom(newProduct);
                 return true;
             }
         }
         return super.add(newProduct);
     }
 
+    @Override
+    public boolean addAll(Collection<? extends Product> products) {
+        boolean changed = false;
+        for (Product product : products) {
+            changed |= add(product);
+        }
+        return changed;
+    }
+
     public Product findByName(String name) {
         for (Product p : this) {
-            if (p.getName().equalsIgnoreCase(name)) {
+            if (p.hasName(name)) {
                 return p;
             }
         }
@@ -34,7 +44,7 @@ public class ProductList extends ArrayList<Product> {
 
         while (it.hasNext()) {
             Product p = it.next();
-            if (p.getName().equalsIgnoreCase(name)) {
+            if (p.hasName(name)) {
                 it.remove();
                 return p;
             }
